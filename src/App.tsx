ae4318c64feab46c543ee1847fa293bb59a1893f
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-06-16 15:48:26 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link, Route, Routes } from 'react-router-dom';
+import Counter from './features/counter/Counter';
+import { RootState } from './store';
+import Login from './features/auth/Login';
+
+function App(): JSX.Element {
+  const value = useSelector((state: RootState) => state.counter.value);
+  const user = useSelector((state: RootState) => state.auth.login);
+  return (
+    <div className="App">
+      <h1>Счет: {value}</h1>
+      <br />
+      {user && (<h2>Привет, {user}</h2>)}
+      <nav>
+        <Link to="/login">Login</Link>{' '}
+        <Link to="/counter">Counter</Link>
+      </nav>
+      <br />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
