8fc9297831ff0fb72deb824657157cb771aba88c
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-06-20 17:24:34 +0200

    addded draft of Books class

--- src/features/books/Books.tsx
+++ src/features/books/Books.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import bookReducer from './bookReducer';
+// import { RootState } from '../../store';
+// import { title } from 'process';
 
 export default function Books(): JSX.Element {
+    // const books = useSelector((globalState: RootState) => globalState.books);
+    // const dispatch = useDispatch();
+    // const [book, setBook] = useState<string>('');
+    // function addBook(e: SyntheticEvent): void {
+    //     e.preventDefault();
+    //     dispatch({ type: 'book/add', payload: bookReducer });
+    //     setBook('');
+    //   }
     return (
-        <div>List of Books</div>
+        <>
+            <div>List of Books</div>
+            <div>Draft</div>
+            {/* <form onSubmit={addBook}>
+                <input
+                  type="text"
+                  required
+                  placeholder="Book name"
+                  value={title}
+//                  onChange={(e) => setBook(e.target.value)}
+                />
+                <input
+                  type="text"
+                  required
+                  placeholder="Author"
+                  value={author}
+//                  onChange={(e) => setBook(e.target.value)}
+                />
+                <input
+                  type="number"
+                  required
+                  placeholder="Production year"
+                  value={dateOfProduction}
+//                  onChange={(e) => setBook(e.target.value)}
+                />
+                <button type="submit">Add book</button>
+            </form> */}
+        </>
     );
 }
--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,14 @@ import { combineReducers, createStore } from 'redux';
 import authReducer from './features/auth/authReducer';
 import counterReducer from './features/counter/counterReducer';
 import tasksReducer from './features/tasks/tasksReducer';
+import bookReducer from './features/books/bookReducer';
 
 const store = createStore(combineReducers({
     counter: counterReducer,
     auth: authReducer,
     tasks: tasksReducer,
     // products: productsReducer
+    books: bookReducer,
 },));
 
 export default store;
